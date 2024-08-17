8d82e2cb3aa68952883ea852b436d5cd84d35b22
amirkhalili <amir1378khalili@gmail.com>
2024-08-17 15:06:41 +0330

    add user rec to index

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
-// import UserRecordRouter from './userRecord/userRecord.router.js';
+import UserRecordRouter from './userRecord/userRecord.router.js';
 import AuthRouter from './auth/auth.router.js';
 import UserRouter from './user/user.router.js';
 
 export const restRouter = express.Router();
 restRouter.use('/auth', AuthRouter);
-// restRouter.use('/userRecords', UserRecordRouter);
+restRouter.use('/userRecords', UserRecordRouter);
 restRouter.use('/users', UserRouter);
