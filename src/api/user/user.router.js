216ab96aec52aa7b02a3db686b59871ddba7bfd5
amirkhalili <amir1378khalili@gmail.com>
2024-08-17 15:12:17 +0330

    enhance routing and add admin check access to routes

--- src/api/user/user.router.js
+++ src/api/user/user.router.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import UserController from './user.controller.js';
 import UserMiddleware from './user.middleware.js';
+import validateSchema from '../../utils/validateSchema.utils.js';
+import PermissionMiddleware from '../../middlewares/permission.middleware.js';
 import passport from 'passport';
+import { createUserSchema, updateUserSchema } from './user.validation.js';
 
 class UserRouter {
   constructor() {
@@ -12,18 +15,37 @@ class UserRouter {
   initializeRoutes() {
     this.router
       .route('/')
-      .get(passport.authenticate('jwt', { session: false }), UserController.findAll)
+      .get(
+        passport.authenticate('jwt', { session: false }),
+        PermissionMiddleware.checkAdminAccess,
+        UserController.findAll
+      )
       .post(
         passport.authenticate('jwt', { session: false }),
+        PermissionMiddleware.checkAdminAccess,
+        validateSchema(createUserSchema),
         UserMiddleware.checkUnique,
         UserController.create
       );
 
     this.router
       .route('/:id')
-      .get(passport.authenticate('jwt', { session: false }), UserController.findOne)
-      .patch(passport.authenticate('jwt', { session: false }), UserController.edit)
-      .delete(passport.authenticate('jwt', { session: false }), UserController.destroy);
+      .get(
+        passport.authenticate('jwt', { session: false }),
+        PermissionMiddleware.checkAdminAccess,
+        UserController.findOne
+      )
+      .patch(
+        passport.authenticate('jwt', { session: false }),
+        PermissionMiddleware.checkAdminAccess,
+        validateSchema(updateUserSchema),
+        UserController.edit
+      )
+      .delete(
+        passport.authenticate('jwt', { session: false }),
+        PermissionMiddleware.checkAdminAccess,
+        UserController.destroy
+      );
   }
 }
 
